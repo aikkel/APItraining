c7ac1da262d319ba7d645bb3e368e6544c59446f
aikkel <mikk39k8@edu.zealand.dk>
2023-10-16 13:10:14 +0200

    still working on random

--- randomSpell.js
+++ randomSpell.js
@@ -1,30 +1,33 @@
 async function getRandomSpell() {
-    try {
-        const spellsResponse = await fetch("https://www.dnd5eapi.co/api/spells/");
-        const spellsData = await spellsResponse.json();
+    const spellsResponse = await fetch("https://www.dnd5eapi.co/api/spells/");
+    const spellsData = await spellsResponse.json();
     
-        const spells = spellsData.results;
-        const randomIndex = Math.floor(Math.random() * spells.length);
-        const randomSpell = spells[randomIndex];
-      
-        const spellDetailsResponse = await fetch(`https://www.dnd5eapi.co${randomSpell.url}`);
-        const spellDetailsData = await spellDetailsResponse.json();
-      
-        // Get references to the HTML elements where you want to display the data
-        const spellNameElement = document.getElementById('spell-name');
-        const spellDescriptionElement = document.getElementById('spell-description');
-        
-        // Update the content of the HTML elements
-        if (spellNameElement && spellDescriptionElement) {
-            spellNameElement.textContent = `Random Spell: ${spellDetailsData.name}`;
-            spellDescriptionElement.textContent = `Description: ${spellDetailsData.desc}`;
-        } else {
-            console.error('Elements not found in the HTML.');
-        }
-    } catch (error) {
-        console.error('An error occurred while fetching and displaying the random spell:', error);
-    }
+    const spells = spellsData.results;
+    const randomIndex = Math.floor(Math.random() * spells.length);
+    const randomSpell = spells[randomIndex];
+  
+    const spellDetailsResponse = await fetch(`https://www.dnd5eapi.co${randomSpell.url}`);
+    const spellDetailsData = await spellDetailsResponse.json();
+  
+    // Extract additional spell information
+    const spellName = spellDetailsData.name;
+    const spellLevel = spellDetailsData.level;
+    const spellRange = spellDetailsData.range;
+    const spellComponents = spellDetailsData.components;
+    // You can add more properties as needed
+
+    // Update HTML to display the random spell info
+    document.getElementById("spell-name").textContent = `Random Spell: ${spellName}`;
+    document.getElementById("spell-level").textContent = `Level: ${spellLevel}`;
+    document.getElementById("spell-range").textContent = `Range: ${spellRange}`;
+    document.getElementById("spell-components").textContent = `Components: ${spellComponents.join(", ")}`;
+    // You can add more elements to display other properties
+
+    console.log(`Random Spell: ${spellName}`);
+    console.log(`Level: ${spellLevel}`);
+    console.log(`Range: ${spellRange}`);
+    console.log(`Components: ${spellComponents.join(", ")}`);
 }
 
-// Call the function to get a random spell when the page loads
-getRandomSpell();
+// Remove this line to prevent it from running on page load
+// getRandomSpell();
