b6bdecb53c17ce943e11092c91a8a2c18d8461d5
aikkel <mikk39k8@edu.zealand.dk>
2023-10-16 13:08:02 +0200

    random spell now works, but only pulls description

--- randomSpell.js
+++ randomSpell.js
@@ -1,22 +1,30 @@
 async function getRandomSpell() {
-    const spellsResponse = await fetch("https://www.dnd5eapi.co/api/spells/");
-    const spellsData = await spellsResponse.json();
-  
-    const spells = spellsData.results;
-    const randomIndex = Math.floor(Math.random() * spells.length);
-    const randomSpell = spells[randomIndex];
-  
-    const spellDetailsResponse = await fetch(`https://www.dnd5eapi.co${randomSpell.url}`);
-    const spellDetailsData = await spellDetailsResponse.json();
-  
-    const spellName = spellDetailsData.name;
-    const spellDescription = spellDetailsData.desc;
-  
-    const spellDetailsElement = document.getElementById("spellDetails");
-    spellDetailsElement.innerHTML = `<h2>Random Spell: ${spellName}</h2><p>Description: ${spellDescription}</p>`;
-  }
-  
-  // Attach the getRandomSpell function to the button click event
-  const generateSpellButton = document.getElementById("generateSpellButton");
-  generateSpellButton.addEventListener("click", getRandomSpell);
-  
+    try {
+        const spellsResponse = await fetch("https://www.dnd5eapi.co/api/spells/");
+        const spellsData = await spellsResponse.json();
+    
+        const spells = spellsData.results;
+        const randomIndex = Math.floor(Math.random() * spells.length);
+        const randomSpell = spells[randomIndex];
+      
+        const spellDetailsResponse = await fetch(`https://www.dnd5eapi.co${randomSpell.url}`);
+        const spellDetailsData = await spellDetailsResponse.json();
+      
+        // Get references to the HTML elements where you want to display the data
+        const spellNameElement = document.getElementById('spell-name');
+        const spellDescriptionElement = document.getElementById('spell-description');
+        
+        // Update the content of the HTML elements
+        if (spellNameElement && spellDescriptionElement) {
+            spellNameElement.textContent = `Random Spell: ${spellDetailsData.name}`;
+            spellDescriptionElement.textContent = `Description: ${spellDetailsData.desc}`;
+        } else {
+            console.error('Elements not found in the HTML.');
+        }
+    } catch (error) {
+        console.error('An error occurred while fetching and displaying the random spell:', error);
+    }
+}
+
+// Call the function to get a random spell when the page loads
+getRandomSpell();
