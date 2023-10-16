ac989f9efc32b11cbf659b9496774b9766aa0ed4
aikkel <mikk39k8@edu.zealand.dk>
2023-10-16 13:27:28 +0200

    description is now in

--- randomSpell.js
+++ randomSpell.js
@@ -1,30 +1,27 @@
 async function getRandomSpell() {
     const spellsResponse = await fetch("https://www.dnd5eapi.co/api/spells/");
     const spellsData = await spellsResponse.json();
-    
+
     const spells = spellsData.results;
     const randomIndex = Math.floor(Math.random() * spells.length);
     const randomSpell = spells[randomIndex];
-  
+
     const spellDetailsResponse = await fetch(`https://www.dnd5eapi.co${randomSpell.url}`);
     const spellDetailsData = await spellDetailsResponse.json();
-  
-    // Extract additional spell information
+
+    // Extract all the spell information
     const spellName = spellDetailsData.name;
     const spellLevel = spellDetailsData.level;
     const spellRange = spellDetailsData.range;
-    const spellComponents = spellDetailsData.components;
+    const spellComponents = spellDetailsData.components ? spellDetailsData.components.join(", ") : "N/A";
+    const spellDescription = spellDetailsData.desc ? spellDetailsData.desc.join("<br>") : "No description available";
     // You can add more properties as needed
 
     // Update HTML to display the random spell info
-    document.getElementById("spell-name").textContent = `Random Spell: ${spellName}`;
+    document.getElementById("spell-name").innerHTML = `<h2>${spellName}</h2>`;
     document.getElementById("spell-level").textContent = `Level: ${spellLevel}`;
     document.getElementById("spell-range").textContent = `Range: ${spellRange}`;
-    document.getElementById("spell-components").textContent = `Components: ${spellComponents.join(", ")}`;
+    document.getElementById("spell-components").textContent = `Components: ${spellComponents}`;
+    document.getElementById("spell-description").innerHTML = `Description: ${spellDescription}`;
     // You can add more elements to display other properties
-
-    console.log(`Random Spell: ${spellName}`);
-    console.log(`Level: ${spellLevel}`);
-    console.log(`Range: ${spellRange}`);
-    console.log(`Components: ${spellComponents.join(", ")}`);
 }
